94e843e74dd32e879d51a9d14d811cef8a550ac6
Zaky Hanif <jakiajjh48@gmail.com>
2021-04-14 15:46:25 +0700

    penambahan search

--- app/src/main/java/com/example/githubuser/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.githubuser
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubuser.adapterdata.Data
 import com.example.githubuser.adapterdata.ListViewAdapter
@@ -47,5 +54,33 @@ class MainActivity : AppCompatActivity() {
         rv_list.layoutManager = LinearLayoutManager(this)
         val listAdapter = ListViewAdapter(this@MainActivity, list)
         rv_list.adapter = listAdapter
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu2 -> {
+                val i = Intent(this, MenuActivity::class.java)
+                startActivity(i)
+                return true
+            }
+            else -> return true
+        }
     }
 }
